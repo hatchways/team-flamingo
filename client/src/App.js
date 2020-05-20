6e511a4d8eb2f5e73340c96e8abe4c14427a9e5a
mgilbert <mgilbert15@simons-rock.edu>
2020-05-19 20:19:44 -0400

    very rough dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import { theme } from "./themes/theme";
 import LandingPage from "./pages/Landing";
+import UserDashboard from "./pages/Dashboard";
 
 import "./App.css";
 
@@ -11,7 +12,9 @@ function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Route path="/" component={LandingPage} />
+        <Switch>
+          <Route path="/profile" component={UserDashboard} />
+        </Switch>
       </BrowserRouter>
     </MuiThemeProvider>
   );
