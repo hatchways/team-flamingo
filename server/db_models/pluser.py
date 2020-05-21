68b5e2ec1be11ed2acbc9fa28069c225c5bb9890
Taylor McLean <taylor@taylors-MBP>
2020-05-21 14:50:02 -0400

    Added registration and login backend functionality
    Several packages were pulled in to accomplish the functionality, including:
    -flask_bcrypt (password hashing)
    -flask_jwt_extended (jwt functionality)
    Validation was added to the username and login_email fields in the user model for data integrity. The server Dockerfile was changed to check for any changes to the db_models, generate new migration scripts, and update the database on container startup.

--- server/db_models/pluser.py
+++ server/db_models/pluser.py
@@ -1,13 +1,42 @@
+import re
 from app import db
+from app import bcrypt
+from sqlalchemy.orm import validates
 
 # Mock User class for testing.
 class plUser(db.Model):
     __tablename__ = "plUser" # use CamelCase for table names
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    login_email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    location = db.Column(db.String(64))
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    login_email = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    location = db.Column(db.String(64), nullable=True)
 
     def __repr__(self):
-        return '<plUser {}>'.format(self.username) 
+        return '<plUser {}>'.format(self.username)
+    
+    def set_password(self, password):
+        if not password:
+            raise AssertionError('No password provided')
+        if len(password) < 6 or len(password) > 64:
+            raise AssertionError('Password must be between 6 and 64 characters')
+
+        hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self.password_hash = hash.decode('utf-8')
+
+    @validates('username')
+    def validate_username(self, key, username):
+        if len(username) < 3 or len(username) > 64:
+            raise AssertionError('Username must be between 5 and 20 characters')
+
+        return username
+
+    @validates('login_email')
+    def validate_email(self, key, login_email):
+        if len(login_email) > 64:
+            raise AssertionError('Email must be under 64 characters')
+        if not re.match(r'[^@]+@[^@]+\.[^@]+', login_email):
+            raise AssertionError('Email is not a valid format')
+
+        return login_email
+
---
+++ server/util/validation_decorators/validate_registration.py
@@ -0,0 +1,49 @@
+import re
+from db_models.pluser import plUser
+from flask import jsonify, request
+from functools import wraps
+from app import db
+
+def validate_registration(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        data = request.get_json()
+
+        # Validate username
+        try:
+            username = data['username']
+        except KeyError:
+            return jsonify({'error': 'Username is required'}), 400
+
+        if len(username) < 3 or len(username) > 64:
+            return jsonify({'error': 'Username must be between 3 and 64 characters'}), 400
+        if db.session.query(plUser.id).filter_by(username=username).scalar() is not None:
+            return jsonify({'error': 'Username is not unique'}), 400
+
+        # Validate login email
+        try:
+            login_email = data['login_email']
+        except KeyError:
+            return jsonify({'error': 'Login email is required'}), 400
+        
+        if not re.fullmatch(r'\w+[.|\w]\w+@\w+[.]\w+[.|\w+]\w+', login_email):
+            return jsonify({'error': 'Email is not a valid format'}), 400
+        if db.session.query(plUser.id).filter_by(login_email=login_email).scalar() is not None:
+            return jsonify({'error': 'Login email is not unique'}), 400
+
+        # Validate password
+        try:
+            password = data['password']
+            confirm = data['confirm']
+        except KeyError:
+            return jsonify({'error': 'Password is required'}), 400
+    
+        if len(password) < 6 or len(password) > 64:
+            return jsonify({'error': 'Password must be between 6 and 64 characters'}), 400
+        
+        if not password == confirm:
+            return jsonify({'error': 'Passwords must match'}), 400
+        
+        return f(*args, **kwargs)
+
+    return wrapper
